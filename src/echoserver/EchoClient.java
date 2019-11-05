7475c83967e668edd4cb494bd6c8871e825db8a9
ColtDahl <dahlcolt@gmail.com>
2019-11-05 11:23:51 -0600

    Implement previous server/client code, with beginnings of multithreading

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,22 +1,66 @@
 package echoserver;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.net.*;
+import java.io.*;
+;
 import java.net.Socket;
 
-public class EchoClient {
+public class EchoClient implements Runnable {
 	public static final int PORT_NUMBER = 6013;
+	public static String server;
 
 	public static void main(String[] args) throws IOException {
+		// Use "127.0.0.1", i.e., localhost, if no server is specified.
+		if (args.length == 0) {
+			server = "127.0.0.1";
+		} else {
+			server = args[0];
+		}
 		EchoClient client = new EchoClient();
 		client.start();
 	}
 
+	public void run(){
+
+	}
+
 	private void start() throws IOException {
-		Socket socket = new Socket("localhost", PORT_NUMBER);
-		InputStream socketInputStream = socket.getInputStream();
-		OutputStream socketOutputStream = socket.getOutputStream();
+
+		Thread clientThreadIn = new Thread(new EchoServer());
+		Thread clientThreadOut = new Thread(new EchoServer());
+
+		clientThreadIn.start();
+		clientThreadOut.start();
+
+		try {
+
+			Socket socket = new Socket("localhost", PORT_NUMBER);
+			int thebyte; //byte wasn't allowed ;(
+
+			InputStream input = socket.getInputStream();
+			OutputStream output = socket.getOutputStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+			while ((thebyte = System.in.read()) != -1) {
+				output.write(thebyte);
+				System.out.write(input.read());
+			}
+
+
+			socket.close();
+
+
+		}
+		catch (
+				ConnectException ce) {
+			System.out.println("We were unable to connect to " + server);
+			System.out.println("You should make sure the server is running.");
+		} catch (
+				IOException ioe) {
+			System.out.println("We caught an unexpected exception");
+			System.err.println(ioe);
+		}
+
 
 		// Put your code here.
 	}
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -6,19 +6,44 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class EchoServer {
-	
+public class EchoServer implements Runnable {
+
 	// REPLACE WITH PORT PROVIDED BY THE INSTRUCTOR
-	public static final int PORT_NUMBER = 0; 
+	public static final int PORT_NUMBER = 6013;
 	public static void main(String[] args) throws IOException, InterruptedException {
 		EchoServer server = new EchoServer();
 		server.start();
 	}
 
+	public void run(){
+
+	}
+
+
 	private void start() throws IOException, InterruptedException {
 		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
 		while (true) {
-			Socket socket = serverSocket.accept();
+			Socket client = serverSocket.accept();
+			Thread serverThread = new Thread(new EchoServer());
+			serverThread.start();
+
+			OutputStream output = client.getOutputStream();
+			InputStream input = client.getInputStream();
+
+
+			while(1==1) { //do always, this is a server
+				int request = client.getInputStream().read(); //get the request from client's input
+				if (request != -1) { //if the request is readable, print it to user
+					output.write(request);
+				}
+				else { //if it isn't, don't, and get rid of buffer
+					output.flush();
+					break;
+				}
+			}
+
+			// Close the client socket since we're done.
+			client.close();
 
 			// Put your code here.
 			// This should do very little, essentially:
